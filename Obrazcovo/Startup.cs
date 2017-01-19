c30cd5cf1cbda84c54c07884a0fadc3184b4379d
Konstantin Antsiferov <kanciferov@mail.ru>
2017-01-19 11:27:32 +0300

    Добавьте файлы проекта.

---
+++ Obrazcovo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Obrazcovo.Startup))]
+namespace Obrazcovo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
